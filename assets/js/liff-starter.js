f98f6470d67e8aece4224eaf1b2aacad742e6665
ChengShihKun <47368327+ChengShihKun@users.noreply.github.com>
2023-08-16 00:01:16 +0800

    Update liff-starter.js
    這裡不確定要不要蓋 App url

--- assets/js/liff-starter.js
+++ assets/js/liff-starter.js
@@ -265,7 +265,7 @@ function sendLiffMessage() {
                         aspectMode: "cover",
                         action: {
                             type: "uri",
-                            uri: "line://app/1657024923-2r46WKKN?auto=yes&type=image&downloadUrl=" + profile.pictureUrl + "&previewUrl=" + profile.pictureUrl
+                            uri: "line://app/2000421497-X3qaQeZ7?auto=yes&type=image&downloadUrl=" + profile.pictureUrl + "&previewUrl=" + profile.pictureUrl
                         }
                     },
                     body: {
@@ -317,7 +317,7 @@ function sendLiffMessage() {
                                 action: {
                                     type: "uri",
                                     label: "Profile",
-                                    uri: "line://app/1657024923-2r46WKKN?type=profile"
+                                    uri: "line://app/2000421497-X3qaQeZ7?type=profile"
                                 },
                                 flex: 2
                             },
